dd5fe59355aecc5e52dca882c5a96e216f58fbf5
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2022-06-13 12:47:05 +0530

    Auth Modal

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,27 @@
 import whiteLogo from "../images/white-tinder-logo.png";
 import colorLogo from "../images/tinder-logo.png";
 
-const Nav = ({ minimal, authToken }) => {
+const Nav = ({ minimal, authToken, setShowModal, showModal, setIsSignUp }) => {
+  const handleClick = () => {
+    setShowModal(true);
+    setIsSignUp(false);
+  };
+
   return (
     <nav>
       <div className="logo-container">
         <img className="logo" src={minimal ? colorLogo : whiteLogo} alt="" />
       </div>
 
-      {!authToken && !minimal && <button className="nav-button">Log in</button>}
+      {!authToken && !minimal && (
+        <button
+          className="nav-button"
+          onClick={handleClick}
+          disabled={showModal}
+        >
+          Log in
+        </button>
+      )}
     </nav>
   );
 };
